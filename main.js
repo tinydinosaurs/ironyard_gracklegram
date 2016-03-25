b14ca6a6827d3eb9991c6a5a67e66d7e38df3961
Dana <dana.lachman@gmail.com>
2016-03-25 16:24:52 -0500

    working form, need to validate and style

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
 // variable for url of photo collection
 var appURL = 'http://small-tiyfe.herokuapp.com/collections/insta-dana'
 
-// storing user input in two variables, one for each input field
-var imageInput = $('.image-url');
-var captionInput = $('.caption');
 
+$('form').submit(function(e) {
+	e.preventDefault();
+	// create object containing user input as values
+	
 
-
-// console.log(userInput)
-// create array to store user input objects
-var imageCollection  = [];
-
-function collectImageEntries(entry) {
-	imageCollection.push(entry);
-	// console.log(imageCollection);
-}
-
-// settings for POST
-var postSettings = {
-	url: appURL,
-	type: 'POST',
-	dataType: 'json',
-	success: function(data) {
-		console.log(data);
-	},
-	error: function(err) {
-		console.log('something went horribly wrong here.');
+	var userInput = {
+	image:  $('.image-url').val(),
+	caption:  $('.caption').val()
 	}
-}
+	
+	$('.image-url').val('');
+	$('.caption').val('');
 
-// $.ajax(postSettings);
+	$.ajax({
+		url: appURL,
+		type: 'POST',
+		data: userInput,
+		dataType: 'json',
+		success: function(data) {
+			//console.log(data);
+		},
+		error: function(err) {
+			console.log('something went horribly wrong here.');
+		} 
+	});
+
+	$('section').html('');
+	$.ajax(getSettings);
+
+});
 
 // settings for GET
 var getSettings = {
@@ -37,25 +39,46 @@ var getSettings = {
 	type: 'GET',
 	dataType: 'json',
 	success: function(data) {
-		console.log(data)
+		//console.log(data);
+		data.forEach(function(imageObject) {
+			var container = $('<div class="image-content"></div>');
+			var instaImage = $('<img>', {src: imageObject.image});
+			var instaCaption = $('<p class="image-caption"></p>').html(imageObject.caption);
+		
+			// console.log(container, instaImage, instaCaption);
+			$('section').append(container);
+			container.append(instaImage);
+			container.append(instaCaption);
+		});
+
+		
 	},
 	error: function(err) {
 		console.log('something went horribly wrong here.')
 	}
 }
 
-// make it happen!
 $.ajax(getSettings);
 
-$('form').submit(function(e) {
-	e.preventDefault();
-	// create object containing user input as values
-	var userInput = {
-	image: imageInput.val(),
-	caption: captionInput.val()
-}
-	collectImageEntries(userInput);
-	console.log(userInput);
 
-});
+// console.log(userInput)
+// create array to store user input objects
+// var imageCollection  = [];
 
+// function collectImageEntries(entry) {
+// 	imageCollection.push(entry);
+// 	// console.log(imageCollection);
+// }
+
+
+
+
+
+// var imageURL = $('.image-url');
+// var imageCaption =   $('.caption');
+// var urlError = $('#URL-error');
+// var captionError = $('caption-error');
+
+// function validationError() {
+// 	if()
+// }
